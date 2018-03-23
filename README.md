6cedaf443ae1784711ba71c8a58f72d10d06a138
kolotaev <kolotaev@tut.by>
2018-03-23 15:02:41 +0300

    run addintions

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Benchmark various web-frameworks for machines with limited resources
 
 - [Frameworks](#frameworks)
-  - []
+  - [Go. Echo.](#go-echo)
 - [Run](#run)
 
 
 ## Frameworks
+
+### Go. Echo.
+
 
 ## Run
 
--- run.sh
+++ run.sh
@@ -1,1 +1,18 @@
-docker run --rm -it image -p 8080:8080
+while getopts i:d: option do
+	case "${option}"
+	in
+	i) IMAGE=${OPTARG};;
+	d) DETACHED=${OPTARG};;
+	esac
+done
+
+NAME="${IMAGE}_cont"
+
+CMD = "docker run --name=$NAME --rm "
+if [[ -nz $DETACHED ]] then;
+	CMD = "${CMD} -d "
+fi
+
+CMD = "${CMD} -d $IMAGE -p 8080:8080"
+
+eval ${CMD}
