8aed3a7429f9822e7b436ef190d927345851bec0
kolotaev <kolotaev@tut.by>
2018-04-06 10:45:34 +0300

    fixes for runner

--- runner.py
+++ runner.py
@@ -2,21 +2,21 @@ import sys
 from contextlib import contextmanager
 
 import pexpect
-from pexpect import EOF
 
 
-COMMAND_PROMPT = 'vagrant@lamp:~\$'
+PROMPT = 'vagrant@lamp:~\$'
+WRK_CMD = 'wrk -t12 -c400 -d300s http://lamp:8080/db'
 
-# fout = open('mylog.txt', 'a')
+# fout = open('runner_log.txt', 'a')
 fout = sys.stdout
 
 
 @contextmanager
 def vagrant():
     try:
-        pexpect.run('vagrant up')
+        pexpect.run('vagrant up', timeout=300)
         child = pexpect.spawnu('vagrant ssh')
-        child.expect(COMMAND_PROMPT)
+        child.expect(PROMPT)
 
         # child.logfile_read = fout
         child.setecho(False)
@@ -26,25 +26,29 @@ def vagrant():
         pexpect.run('vagrant halt')
 
 
-def action(child):
-    child.sendline('ls -la')
-    child.expect(COMMAND_PROMPT)
+def run(s):
+    s.sendline('ls -la')
+    s.expect(PROMPT)
 
-    child.sendline('free -m')
-    child.expect(COMMAND_PROMPT)
-    print(child.before)
+    s.sendline('free -m')
+    s.expect(PROMPT)
+    print(s.before)
 
-    child.sendline('df')
-    child.expect(COMMAND_PROMPT)
-    print(child.before)
+    s.sendline('/bin/bash', ["grep 'cpu ' /proc/stat | awk '{print ($2+$4)*100/($2+$4+$5)}'"])
+    s.expect(PROMPT)
+    print(s.before)
 
-    child.sendline('exit')
-    index = child.expect([pexpect.EOF, "(?i)there are stopped jobs"])
+    s.sendline('df')
+    s.expect(PROMPT)
+    print(s.before)
+
+    s.sendline('exit')
+    index = s.expect([pexpect.EOF, "(?i)there are stopped jobs"])
     if index == 1:
-        child.sendline("exit")
-        child.expect(EOF)
+        s.sendline("exit")
+        s.expect(pexpect.EOF)
 
 
 if __name__ == '__main__':
     with vagrant() as ssh:
-        action(ssh)
+        run(ssh)
