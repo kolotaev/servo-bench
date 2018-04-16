85f8d6925de4566fd7c650a811392cf0fea4ce51
kolotaev <kolotaev@tut.by>
2018-04-16 19:19:27 +0300

    add clojure json api

--- clojure-ring/project.clj
+++ clojure-ring/project.clj
@@ -2,7 +2,8 @@
   :description "Ring application for testing stress load"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-jetty-adapter "1.6.3"]]
+                 [ring/ring-jetty-adapter "1.6.3"]
+                 [cheshire "5.8.0"]]
   :main serv.core
   :aot [serv.core]
 ;  :jvm-opts ["-Xmx100m" "-server"]
--- clojure-ring/src/serv/core.clj
+++ clojure-ring/src/serv/core.clj
@@ -2,19 +2,51 @@
     (:use compojure.core
           [ring.adapter.jetty :only [run-jetty]])
     (:require [compojure.route :as route]
-              [compojure.handler :as handler])
+              [compojure.handler :as handler]
+              [cheshire.core :as json])
     (:gen-class))
 
 
+(defn rand-string
+  ([n]
+   (let [chars  (map char (range 65 90))
+         genned (take n (repeatedly #(rand-nth chars)))]
+     (reduce str genned))))
+
+
+(defn create-user
+  [friend]
+  {:name (rand-string 10)
+   :surname (rand-string 3)
+   :street (rand-string 15)
+   :school (rand-string 9)
+   :bank (rand-string 4)
+   :a (rand-int 100)
+   :b (rand)
+   :c (rand-int 1090)
+   :friend friend})
+
+(defn json-endpoint
+  []
+  (let [friend (create-user nil)]
+    (create-user friend)))
+
+
+(defn db-endpoint
+  [])
+
+
+(defn root-endpoint
+  []
+  "<html>It's me, Ring App.<br/>Use routes:<br/><a href='./json'>json</a><br/><a href='./db'>db</a></html>")
 
 
 (defroutes main-routes
-  (GET "/" [] "Powered by foo")
-  (GET "/json" (json-endpoint))
-  (GET "/db" (db-endpoint))
-
+  (GET "/" [] (root-endpoint))
+  (GET "/json" [] (json/encode (json-endpoint)))
+  (GET "/db" [] (json/encode (db-endpoint)))
   (route/resources "/")
-  (route/not-found "Page not found"))
+  (route/not-found "Sorry, page not found"))
 
 (def app
   (handler/api main-routes))
