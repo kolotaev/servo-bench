4429e4b560a085b431345a1017d7cd5b6e624bce
kolotaev <kolotaev@tut.by>
2020-02-12 20:53:42 +0300

    Complete synchrony

--- ruby-sinatra-synchrony/app.rb
+++ ruby-sinatra-synchrony/app.rb
@@ -1,5 +1,32 @@
 require 'json'
 
+
+def random_string(length)
+  rand(36**length).to_s(36)
+end
+
+def create_user
+  User.new(friend: User.new)
+end
+
+class User
+  def initialize(opts = {})
+    @name = opts.fetch(:name, random_string(10))
+    @surname = opts.fetch(:surname, random_string(3))
+    @street = opts.fetch(:street, random_string(15))
+    @school = opts.fetch(:school, random_string(9))
+    @bank = opts.fetch(:bank, random_string(4))
+    @a = opts.fetch(:a, rand(0..100))
+    @b = opts.fetch(:b, rand)
+    @c = opts.fetch(:c, rand(0..100))
+    @friend = opts.fetch(:friend, nil)
+  end
+
+  def to_json(*options)
+    instance_variables.map { |v| [v.to_s[1..-1].to_sym, instance_variable_get(v)] }.to_h.to_json
+  end
+end
+
 class App < Sinatra::Base
   register Sinatra::Synchrony
 
@@ -15,16 +42,24 @@ class App < Sinatra::Base
       <a href='./delay/:n'>delay for n seconds</a></html>)
   end
 
+  get '/json' do
+    create_user.to_json
+  end
+
+  get '/db' do
+    qry = "SELECT pg_sleep(#{Random.rand(SLEEP_MAX)})"
+    DB.query(qry) do |result|
+      users = []
+      LOOP_COUNT.times do
+        users << create_user
+      end
+      { db_query: qry, data: users, result: result.first }.to_json
+    end
+  end
+
   # This endpoint doesn't participate in the benchmark, but present here as an example of sleep
   get '/delay/:n' do |n|
     EM::Synchrony.sleep n.to_i
     "delayed for #{n} seconds".to_json
-  end
-
-  get '/db' do
-    n = rand(0...2.0)
-    DB.query("SELECT NOW(), #{1} as sl, pg_sleep(#{1})") do |result|
-      result.first.to_json
-    end
   end
 end
--- ruby-sinatra-synchrony/config.ru
+++ ruby-sinatra-synchrony/config.ru
@@ -5,7 +5,7 @@ DB = EM::Synchrony::ConnectionPool.new(size: 450) do
   PG::EM::Client.new(host: '127.0.0.1', port: 5432, user: 'postgres', password: 'root', dbname: 'postgres')
 end
 
-SLEEP_MAX = (ENV.fetch 'SQL_SLEEP_MAX', 0).to_i
+SLEEP_MAX = (ENV.fetch 'SQL_SLEEP_MAX', 0).to_f
 LOOP_COUNT = (ENV.fetch 'LOOP_COUNT', 0).to_i
 
 run App
