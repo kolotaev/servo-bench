bbe1a8d74aca2ba02e1d23d4a02733e432df57dd
kolotaev <kolotaev@tut.by>
2020-02-19 09:18:24 +0300

    Crystal add user

--- crystal-kemal/server.cr
+++ crystal-kemal/server.cr
@@ -1,14 +1,33 @@
 require "kemal"
 
-# Matches GET "http://host:port/"
-get "/" do
-  "Hello World!"
+
+def random_string(length)
+  rand(36**length).to_s(36)
 end
 
-# Creates a WebSocket handler.
-# Matches "ws://host:port/socket"
-ws "/socket" do |socket|
-  socket.send "Hello from Kemal!"
+class User
+  def initialize(**opts)
+    @name = opts.fetch(:name, random_string 10)
+    @surname = opts.fetch(:surname, random_string 3)
+    @street = opts.fetch(:street, random_string 15)
+    @school = opts.fetch(:school, random_string 9)
+    @bank = opts.fetch(:bank, random_string 9)
+    @a = opts.fetch(:a, rand(0..100))
+    @b = opts.fetch(:b, rand)
+    @friend = opts.fetch(:friend, nil)
+  end
+end
+
+get "/" do
+  "
+  <html>It's me, Express App.<br/>
+  Use routes:<br/><a href='./json'>json</a><br/>
+  <a href='./db'>db</a></html>
+  "
+end
+
+get "/json" do
+  random_string 5
 end
 
 Kemal.run
